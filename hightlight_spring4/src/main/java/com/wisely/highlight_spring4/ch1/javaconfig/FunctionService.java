62fcd613698673d6c9286e2637fc97bf97282ba3
xuyz_feisjy <xuyanzhe@hotmail.com>
2019-12-10 22:12:25 +0800

    删除hightlight_spring4文件

--- hightlight_spring4/src/main/java/com/wisely/highlight_spring4/ch1/javaconfig/FunctionService.java
+++
@@ -1,9 +0,0 @@
-package com.wisely.highlight_spring4.ch1.javaconfig;
-
-public class FunctionService {
-	
-	public String SayHello(String word){
-		return "Hello "+word+"!";
-	}
-	
-}
--- hightlight_spring4/src/main/java/com/wisely/highlight_spring4/ch2/scope/DemoSingletonService.java
+++
@@ -1,7 +0,0 @@
-package com.wisely.highlight_spring4.ch2.scope;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class DemoSingletonService {
-}
